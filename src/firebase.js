47cdb0b1b6851c1ea21d476b818abd2c44b49231
RagdollPictures <emelie.falk.renstrom@gmail.com>
2024-03-25 12:43:41 +0100

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4JAj8qof6MXymQraKW_gEtbr38cKY5OI",
+  authDomain: "ragdollmovies.firebaseapp.com",
+  projectId: "ragdollmovies",
+  storageBucket: "ragdollmovies.appspot.com",
+  messagingSenderId: "378700446428",
+  appId: "1:378700446428:web:a22400e639fc2ae2591219"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import Collection from "../components/Collection";
+function Home() {
+  
+  return (
+    <div>
+      <Collection />
+    </div>
+  );
+}
+
+export default Home;
